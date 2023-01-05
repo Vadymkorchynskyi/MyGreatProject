7cafc05f9775b63f130d2653108431187bb98877
Vadimir <vadymir@MacBook-Air-Vadimir.local>
2023-01-05 15:57:51 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    // здесь будет проект, который я еще не придумал. Хотя есть задумки
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
